1ff996c242fa6e5b3877d091949347f36e92c300
Eugen Ursu <e.ursu@campus.fct.unl.pt>
2022-05-22 00:48:17 +0100

    added corpus from zip and elim freq = 1

--- localmaxs.py
+++ localmaxs.py
@@ -5,21 +5,23 @@ Created on Tue May  3 18:54:37 2022
 @author: eugen
 """
 import re
+import os
 import numpy as np
+import pickle
+from zipfile import ZipFile
 from sklearn.feature_extraction.text import CountVectorizer
 
-corpus = ['Knight Lore é um jogo eletrônico de ação e aventura desenvolvido e lançado pela Ultimate Play the Game para o ZX Spectrum em 1984. O jogo é conhecido pelo uso de gráficos isométricos, que popularizou ainda mais em jogos eletrônicos. Em Knight Lore, Sabreman tem quarenta dias para coletar objetos em um castelo e preparar uma cura para sua maldição do lobisomem. Cada sala do castelo é representada em monocromático em sua própria tela e consiste em blocos para escalar, obstáculos para evitar e quebra-cabeças para resolver.',
-          'Hello there General Kenobi.']
+corpus = []
+
+
+archive = ZipFile('corpus2mw.zip', 'r')
+
+fileList = archive.namelist()
+for file in fileList:
+    corpus.append((archive.read(file)).decode('UTF-8'))
 
 
 regexp = re.compile('[a-zA-Z0-9 éáíúóãõçôâ-]') 
-
-'''
-import os
-for filename in os.listdir():
-   with open(os.path.join(os.getcwd(), filename), 'r') as f:
-       corpus.append(f.read())
-'''
 
 def dice(freq, pref_freqs, suff_freqs):
     pref_freqs = list(pref_freqs)
@@ -37,17 +39,29 @@ def processText(corpus):
         listT = list(text)
         i = 0
         for  c in listT:
+            print(c)
             if not regexp.search(c) and not listT[i-1]==' ':
                 listT.insert(i, ' ')
             i +=1
         corp.append(''.join(listT))
     return corp
 
-corpus =  processText(corpus)
+readPickle = False
+if readPickle:
+    with open('corpusList', 'rb') as fp:
+        n_list = pickle.load(fp)
+else: 
+    corpus =  processText(corpus)
+
+with open('corpusList', 'wb') as fp:
+        pickle.dump(corpus, fp)
 
 
 vectorizer = CountVectorizer(analyzer='word', ngram_range=(2, 7))
 vec_fit = vectorizer.fit_transform(corpus)
-word_list = vec_fit.get_feature_names_out()
-count_list = np.asarray(vec_fit.sum(axis=0))
+word_list = vectorizer.get_feature_names_out()
+count_list = np.asarray(vec_fit.sum(axis=0))[0]
 freq_dict = dict(zip(word_list,count_list))
+
+freq_dict = {key:val for key, val in freq_dict.items() if val != 1}
+    
